8127bf2ecac7fe38c168c12cd13faea492c6ca70
maicon2022 <maicon.felipe@academico.ifpb.edu.br>
2024-07-17 00:34:26 -0300

    first commit

---
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose.ui.theme.ComposeTheme
+import kotlin.io.encoding.Base64
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeApp()
+                }
+            }
+        }
+    }
+}
+
+// Função que chama a ComposeQuadrante 4 vezes para gerar um layout com 4 quadrantes
+@Composable
+fun ComposeApp() {
+    Column(Modifier.fillMaxWidth()) {
+
+        // Para que os componentes filhos ocupem uma proporção específica do espaço disponível na linha
+        // '1f' -> O componente deve ocupar uma fração igual do espaço disponível em comparação com outros componentes com peso similar.
+        Row(Modifier.weight(1f)) {
+
+            // Quadrante Superior Esquerdo
+            ComposeQuadrante(
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+
+            // Quadrante Superior Direito
+            ComposeQuadrante(
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row(Modifier.weight(1f)) {
+
+            // Quadrante Inferior Esquerdo
+            ComposeQuadrante(
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+
+            // Quadrante Inferior Direito
+            ComposeQuadrante(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+// Função que gera 1 Quadrante
+@Composable
+private fun ComposeQuadrante(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeAppPreview() {
+    ComposeTheme {
+        ComposeApp()
+    }
+}
